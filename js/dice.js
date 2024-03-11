97b3db1b71ecf45be65082b7cf4d9422dafcd7de
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-11 17:37:42 +0100

    Adding elements

--- js/dice.js
+++ js/dice.js
@@ -5,3 +5,18 @@ Prima di partire a scrivere codice poniamoci qualche domanda:
 Che ci sia un array da qualche parte?
 Se dobbiamo confrontare qualcosa che "cosa" ci serve? */
 
+
+let computerNumbar = Math.floor(Math.random() * 6) + 1;
+let button = document.querySelector (".btn");
+let availableNumber = [1, 2, 3, 4, 5, 6];
+console.log(availableNumber);
+button.addEventListener("click", function(){
+   
+    
+
+    let playerNumber = document.getElementById("userValue").value;
+    let tmpHtml = '';
+    for (i = 0; i < availableNumber.length; i++){
+        
+    }
+})
