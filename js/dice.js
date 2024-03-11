5660b4dbff5d7e8a53a67f0f911adb4a51d8988e
ElisaMavilia <elisamavilia1@gmail.com>
2024-03-11 17:58:36 +0100

    Adjustments

--- js/dice.js
+++ js/dice.js
@@ -10,20 +10,25 @@ let computerNumber = Math.floor(Math.random() * 6) + 1;
 let button = document.querySelector (".btn");
 let availableNumber = [1, 2, 3, 4, 5, 6];
 console.log(availableNumber);
+console.log(computerNumber);
 button.addEventListener("click", function(){
    
     let playerNumber = document.getElementById("userValue").value;
     for (i = 0; i < availableNumber.length; i++){
        
         let box = document.getElementById("notification");  
-        if (playerNumber < computerNumber) {
+        if (playerNumber < computerNumber){
         notification.innerHTML="Hai vinto!";
         box.classList.remove("d-none");
         box.style.color="green";
-       } else {
-        notification.innerHTML="Hai perso!";
-        box.classList.remove("d-none");
-        box.style.color="red";
-       }
-    }
+        } else if
+            (playerNumber = computerNumber){;
+            notification.innerHTML="Parità!";
+            box.classList.remove("d-none");
+            box.style.color="green";
+        } else 
+            notification.innerHTML="Hai perso!";
+            box.classList.remove("d-none");
+            box.style.color="red";
+    } 
 })
